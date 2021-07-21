ec5f3fa48216e4555ef05b643986603863952783
SquirrelDiary <932753896@qq.com>
2021-07-21 09:55:52 +0800

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,18 +10,25 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
   ]
+  let copy = Array(anecdotes.length).fill(0)
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(copy)
 
   const handleClick = () => {
-    let value = Math.floor(Math.random() * 7 + 1)
+    let value = Math.floor(Math.random() * 7)
     setSelected(value)
+  }
+  const handleVote = () => {
+    copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
   }
 
   return (
     <div>
-      <div>
-        {anecdotes[selected]}
-      </div>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleClick}>next anecdote</button>
     </div>
   );
